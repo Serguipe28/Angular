0261a39d7a139e5c85f6780ece507ed6cc28d3ee
Sergio Guillen <serguipe28@gmail.com>
2022-02-11 10:55:20 +0100

    add NavBar and Routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,24 +5,36 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from "@angular/common/http";
-import { ClaseListaComponent } from "./clase/clase-lista/clase-lista.component";
+import { RouterModule, Routes } from "@angular/router";
 import { FormsModule } from '@angular/forms';
-import { AlumnosListaComponent } from './alumno/alumnos-lista/alumnos-lista.component'
+ 
+import { ClaseListaComponent } from "./clase/clase-lista/clase-lista.component";
+import { AlumnosListaComponent } from './alumno/alumnos-lista/alumnos-lista.component';
+import { ProfesoresComponent } from './profesores/profesores/profesores.component';
+import { PaginaNoEncontradaComponent } from './otros/pagina-no-encontrada/pagina-no-encontrada.component'
+
+const appRutas: Routes = [
+  { path: 'Alumno', component:AlumnosListaComponent },
+  { path: 'Profesores', component: ProfesoresComponent},
+  { path: '', redirectTo: '/profesores', pathMatch: 'full' },
+  { path: '**', component: PaginaNoEncontradaComponent  }
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     ClaseListaComponent,
     AlumnosListaComponent,
+    ProfesoresComponent,
+    PaginaNoEncontradaComponent,
     
-
-
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(appRutas)
   ],
   providers: [],
   bootstrap: [AppComponent]
